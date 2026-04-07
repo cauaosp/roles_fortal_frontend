b164174d97fd1cb773053dd5fcedebddbfcada94
cauaosp <cauaosp20@gmail.com>
2026-04-07 16:14:14 -0300

    fix: remove .env from .dockerignore allows the frontend to accept enviromment variables

--- roles-fortal/src/api/get_articles.ts
+++ roles-fortal/src/api/get_articles.ts
@@ -2,7 +2,14 @@ import type { Article } from "../data/types";
 
 export default async function getArticles(): Promise<Article[]> {
   try {
-    const response = await fetch("http://localhost:5001/opovo");
+    const backendUrl = import.meta.env.VITE_URL_BACKEND;
+
+    if (!backendUrl) {
+      throw new Error("VITE_URL_BACKEND não definida");
+    }
+
+    const response = await fetch(backendUrl);
+
     return await response.json();
   } catch (error) {
     console.error("Erro ao buscar artigos:", error);
