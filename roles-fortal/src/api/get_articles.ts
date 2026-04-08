2750c9bd9660ce522f43ce69a9035ae7b57066d4
cauaosp <cauaosp20@gmail.com>
2026-04-08 16:00:17 -0300

    using nginx conf to integrate in k8s services

--- roles-fortal/src/api/get_articles.ts
+++ roles-fortal/src/api/get_articles.ts
@@ -2,13 +2,7 @@ import type { Article } from "../data/types";
 
 export default async function getArticles(): Promise<Article[]> {
   try {
-    const backendUrl = import.meta.env.VITE_URL_BACKEND;
-
-    if (!backendUrl) {
-      throw new Error("VITE_URL_BACKEND não definida");
-    }
-
-    const response = await fetch(backendUrl);
+    const response = await fetch("/opovo");
 
     return await response.json();
   } catch (error) {
