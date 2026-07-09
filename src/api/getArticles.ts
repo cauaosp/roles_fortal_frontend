51263e117666f87dde31ec76cf9c60e769c5fe18
cauaosp <cauaosp20@gmail.com>
2026-07-09 13:43:06 -0300

    ajustado fetch para url padrão

--- src/api/getArticles.ts
+++ src/api/getArticles.ts
@@ -1,9 +1,12 @@
 import type { JornalType } from "../data/types";
 
+const DEFAULT_ARTICLES_URL =
+  "https://cauaosp.github.io/roles_fortal_backend/artigos_ceara.json";
+
 export async function getArticles(): Promise<JornalType> {
-  const response = await fetch(
-    "https://cauaosp.github.io/roles_fortal_backend/artigos_ceara.json",
-  );
+  const url = import.meta.env.VITE_ARTICLES_URL ?? DEFAULT_ARTICLES_URL;
+  console.log("url: ", url);
+  const response = await fetch(url);
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
